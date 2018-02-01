c30cf5a697c377348d1dcdaa51c5136ed482f4d3
abdullah <abdullah@abdullahs-Mac.local>
2018-02-01 11:27:44 +0530

    .

--- saloonMaster/Admin/Controller/adminSignUp.swift
+++ saloonMaster/Admin/Controller/adminSignUp.swift
@@ -28,6 +28,7 @@ class adminSignUp: UIViewController, UITextFieldDelegate,UIPickerViewDelegate, U
     @IBOutlet weak var passwordTextFeild: UITextField!
     @IBOutlet weak var usernameTextFeild: UITextField!
     
+    
     let cityValues = ["Bhopal","Bombay","Bangalore","Jharkhand","Kashmir","Jabalpur"]
     let countryValues = ["India","Bangladesh","Nepal","Shrilanka","China"]
     
@@ -35,11 +36,6 @@ class adminSignUp: UIViewController, UITextFieldDelegate,UIPickerViewDelegate, U
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
-        
-        usernameTextFeild.delegate = self
-        
-        addressTextFeild.delegate = self
-        
         cityPickerView.delegate = self
         cityPickerView.dataSource = self
         cityTextFeild.inputView = cityPickerView
@@ -47,6 +43,13 @@ class adminSignUp: UIViewController, UITextFieldDelegate,UIPickerViewDelegate, U
         countryPickerView.delegate = self
         countryPickerView.dataSource = self
         countryTextFeild.inputView = countryPickerView
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            addressTextFeild.resignFirstResponder()
+        }
+        return true
     }
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -91,10 +94,7 @@ class adminSignUp: UIViewController, UITextFieldDelegate,UIPickerViewDelegate, U
         }
     }
     
-    // This function will take care of saving data to firebase and core datadatabase
-    @IBAction func saveCheckOperationButton(_ sender: UIButton) {
-        
-    }
+    
     
     // Alert message which will pop up when user types anything irrelavant to textfields
     func alertMessage(err: String, message: String) {
@@ -115,13 +115,26 @@ class adminSignUp: UIViewController, UITextFieldDelegate,UIPickerViewDelegate, U
     }
     
     // Validating the username textfield from taking correct input
-    @IBAction func isValidInput(_ sender: UITextField) {
-       // let RegEx = "\\A\\w{0,30}\\z"
-        let RegEx = "[A-Z0-9a-z_]{0,30}"
-        let Test = NSPredicate(format:"SELF MATCHES %@", RegEx)
-        if !Test.evaluate(with: sender.text) {
-            self.alertMessage(err: "Error", message: "Special charachters not allowed except underscore or charachters are less than 7.")
+  /*  @IBAction func isValidInput(_ sender: UITextField) {
+        //_ = usernameValidatingWhenSubmit()
+    }*/
+    
+    // Validating username when submit button is pressed
+    func usernameValidatingWhenSubmit() -> Bool {
+        if let username = usernameTextFeild.text{
+            let RegEx = "[A-Z0-9a-z_]{0,30}"
+            if username.rangeOfCharacter(from: .whitespaces) == nil {
+                let Test = NSPredicate(format:"SELF MATCHES %@", RegEx)
+                if !Test.evaluate(with: username) {
+                    self.alertMessage(err: "Error", message: "Special charachters not allowed except underscore or charachters are less than 7.")
+                    return false
+                }
+            }else{
+               self.alertMessage(err: "Error", message: "Whitespace not accepted for username")
+               return false
+            }
         }
+        return true
     }
     
     // When return is pressed from keyboard, resign from first responder
@@ -161,60 +174,132 @@ class adminSignUp: UIViewController, UITextFieldDelegate,UIPickerViewDelegate, U
         return valType
     }
     
-    // Validating password and re-entered password fields
-    @IBAction func rePasswordValueValidation(_ sender: UITextField) {
+    // Validating re-enter password when submit button is pressed
+    func rePasswordValidatingWhenSubmit() -> Bool {
         if !(passwordTextFeild.text?.isEmpty)! {
             let password = passwordTextFeild.text
             if let rePassword = rePasswordTextFeild.text {
                 if !(rePassword.isEmpty) {
                     if !(password == rePassword) {
                         self.alertMessage(err: "Error", message: "Password didn't match")
+                        return false
                     }
                 }else{
                     self.alertMessage(err: "Error", message: "Please enter re-entered password")
+                    return false
                 }
             }
         }else{
             self.alertMessage(err: "Error", message: "Please enter password first")
+            return false
         }
+        return true
     }
     
     // Validating nameTextField
     @IBAction func validatingNameTextFieldValues(_ sender: UITextField) {
-        let regex = "[A-Za-z]{0,30}"
-        if let name = sender.text {
+        _ = validatingNameWhenSubmit()
+    }
+    
+    // Validating name field when submit button is pressed
+    func validatingNameWhenSubmit() -> Bool {
+        if let name = nameTextFeild.text {
+            let regex = "[A-Za-z]{0,30}"
             let verifiedName = NSPredicate(format:"SELF MATCHES %@", regex)
             if !verifiedName.evaluate(with: name) {
                 self.alertMessage(err: "Error", message: "Name text field should contain only alphabatical charachters")
+                return false
             }
         }
+        return true
     }
     
     //Validating contactTextField
     @IBAction func validatingContactTextFieldValues(_ sender: UITextField) {
-        let regex = "[0-9]{0,10}"
-        if let contactNo = sender.text {
-            let verifiedName = NSPredicate(format:"SELF MATCHES %@", regex)
-            if !verifiedName.evaluate(with: contactNo) {
-                self.alertMessage(err: "Error", message: "Please enter 10 digit contact number only")
-            }
+        if let contactNo = contactNoTextFeild.text {
+            _ = validatingContactWhenSubmit(contactNo : contactNo)
         }
+    
     }
-    @IBAction func validatingEmailID(_ sender: UITextField) {
+    
+    // Validating contact no when submit pressed
+    func validatingContactWhenSubmit(contactNo: String) -> Bool {
+        let regex = "[0-9]{0,10}"
+        let verifiedName = NSPredicate(format:"SELF MATCHES %@", regex)
+        if !verifiedName.evaluate(with: contactNo) {
+            self.alertMessage(err: "Error", message: "Please enter 10 digit contact number only")
+            return false
+        }
+        return true
+    }
+    
+    // Validating email
+    /*@IBAction func validatingEmailID(_ sender: UITextField) {
         if let emailValue = sender.text {
-            let regex = "[A-Za-z0-9_.]+@[A-Za-z0-9]\\.[a-zA-Z]{2,6}]{0,}"
-            let emailTestValue = NSPredicate(format: "SELF MATCHES %@", regex)
-            if !emailTestValue.evaluate(with: emailValue) {
-                self.alertMessage(err: "Error", message: "Please enter correct email")
+            _ = validatingEmailWhenSubmit(emailValue: emailValue)
+        }
+    }*/
+    
+    
+    // Validating email when submit button pressed
+    func validatingEmailWhenSubmit(emailValue: String) -> Bool {
+        let regex = "[A-Za-z0-9_.]+@[A-Za-z0-9]+\\.[a-zA-Z]{2,6}]{0,}"
+        let emailTestValue = NSPredicate(format: "SELF MATCHES %@", regex)
+        if !emailTestValue.evaluate(with: emailValue) {
+            self.alertMessage(err: "Error", message: "Please enter correct email")
+            return false
+        }
+        return true
+    }
+    
+    // Validating text view after editing
+    func textViewDidEndEditing(_ textView: UITextView) {
+        _ = addressTextViewValidating()
+    }
+    
+    // Validating address text view when submit
+    func addressTextViewValidating() -> Bool {
+        if (addressTextFeild.text) != nil {
+            let regex = "[A-Za-z0-9./#@]{0,}"
+            let addressValue = addressTextFeild.text
+            let addressEvaluatedValue = NSPredicate(format: "SELF MATCHES %@", regex)
+            if addressEvaluatedValue.evaluate(with: addressValue) != true {
+                self.alertMessage(err: "Error", message: "Please use correct charachters")
+                return false
             }
         }
+        return true
     }
     
     // Validating textfields are emplty or not
-    func viewsValidation(){
+    func viewsValidation() -> Bool {
         if usernameTextFeild.text! == "" || passwordTextFeild.text! == "" || rePasswordTextFeild.text! == "" || nameTextFeild.text! == "" || contactNoTextFeild.text! == "" || emailIDTextFeild.text! == "" || cityTextFeild.text! == "" || addressTextFeild.text! == "" || countryTextFeild.text! == "" {
             alertMessage(err: "Error",message: "Fields are empty !!!")
+            return false
+        }else if usernameValidatingWhenSubmit() == false {
+            return false
+        }else if rePasswordValidatingWhenSubmit() == false {
+            return false
+        }else if validatingNameWhenSubmit() == false {
+            return false
+        }else if validatingContactWhenSubmit(contactNo: contactNoTextFeild.text!) == false {
+            return false
+        }else if validatingEmailWhenSubmit(emailValue: emailIDTextFeild.text!) == false {
+            return false
+        }else if addressTextViewValidating() == false {
+            return false
+        }
+        return true
+    }
+    
+    // This function will take care of saving data to firebase and core datadatabase
+    @IBAction func saveCheckOperationButton(_ sender: UIButton) {
+        if viewsValidation() == false {
+            alertMessage(err: "Error", message: "Please enter correct values")
         }
     }
-}
+    
+    // Dismiss city and country
+   
 
+}
